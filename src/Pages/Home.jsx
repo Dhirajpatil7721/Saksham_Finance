925fc083a8a05f757f5de7a7403bff4f7a88b53a
Dhiraj Patil <dhirajpatil7721@example.com>
2025-08-20 12:24:05 +0530

    ok

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,8 @@ import { motion } from 'framer-motion';
 // import backgroundImage from '../assets/bg2.jpg';
 // import backgroundImage from '../assets/bg3.jpg';
 // import backgroundImage from '../assets/bg4.jpg';
-import backgroundImage from '../assets/bg5.jpg';
+// import backgroundImage from '../assets/bg5.jpg';
+import backgroundImage from '../assets/bg1.png';
 
 function Home() {
   // Animation variants
